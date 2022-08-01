ccdf4f5d0d7f86f9d2d52e188c7e9afce11ac64f
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-08-01 10:41:33 +0530

    redux setup

--- todoApp/src/Redux/store.js
+++ todoApp/src/Redux/store.js
@@ -2,7 +2,7 @@ import { legacy_createStore } from "redux";
 import { todoReducer } from "./reducer";
 
 
-const store=legacy_createStore(todoReducer);
+export const store=legacy_createStore(todoReducer);
 
 console.log(store.getState());
 
